2083bc01efe255d3a2bba53296673384e07250bb
ADOLFO PINEDA <adolfp@uw.edu>
2017-08-11 15:31:46 -0700

    main program that manages the trie

--- t9_predictive_text/t9.c
+++ t9_predictive_text/t9.c
@@ -1,41 +1,37 @@
 
 /*
-the number 0 represents a space
-1 represents a set of symbols such as { , . ! ? } etc
-2 ABC 
-3 DEF 
-4 GHI 
-5 JKL 
-6 MNO
-7 PQRS 
-8 TUV 
-9 WXYZ
+    Adolfo Pineda, CSE 374, 07/20/2017,
+    Homework 5: C data structures.
 
+    This program emulates the T9 predictive
+    text that the first cellphones with text
+    messaging used. Below is a legend of the
+	numbers with their corresponding letters:
+
+	2 ABC 
+	3 DEF 
+	4 GHI 
+	5 JKL 
+	6 MNO
+	7 PQRS
+	8 TUV 
+	9 WXYZ
+    
 */
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#ifndef T9_C
+#define T9_C
 
-// a single list node
-struct Node {
-    char* string;
-    int val;
-    struct Node* link[10];
-};
+#include "header.h"
 
-void makeTree(struct Node*, char*);
-void freeTree(struct Node*);
-struct Node* form_node(int, char*);
-int whichDigit(char letter);
-void findWord(struct Node*, char*);
-char* trim(char* s);
-
+// Uses a dictionary (list of words) to build and run
+// the t9 predictive text program.
 int main(int argc, char** argv) {
     FILE* inputFile;
     inputFile = fopen(argv[1], "r");
-    struct Node * root = form_node(20, NULL);
+    struct Node * root = form_node(NULL);
     char str[100];
+    // Creates the tree using the passed in dictionary
     while ((fgets(str, 100, inputFile)) != NULL) {
         char* strTrim = trim(str);
         makeTree(root, strTrim);
@@ -69,76 +65,7 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-
-void makeTree(struct Node* root, char* word) {
-    struct Node* temp = root;
-    for (int i = 0; i < strlen(word); i++) {
-        int val = whichDigit(word[i]);
-        if (temp->link[val] == NULL) {
-            if (i == strlen(word) - 1) {
-                temp->link[val] = form_node(val, word);
-            } else {
-                temp->link[val] = form_node(val, NULL);
-            }
-            temp = temp->link[val];
-        } else {
-            if (i == strlen(word) - 1 && temp->link[val]->string == NULL) {
-                temp->link[val]->string =
-                    (char*)malloc(sizeof(char)*strlen(word));
-                strncpy(temp->link[val]->string, word, strlen(word));
-            } else if (i == strlen(word) - 1 &&
-                (strcmp(temp->link[val]->string, word) != 0)) {
-                temp = temp->link[val];
-                while (temp->link[0] != NULL) {
-                    temp = temp->link[0];
-                }
-                temp->link[0] = form_node(20, word);
-            } else {
-                temp = temp->link[val];
-            }
-        }
-    }
-}
-
-// Print lst to stdout
-void findWord(struct Node* root, char* word) {
-    struct Node* temp = root;
-    for (int i = 0; i < strlen(word); i++) {
-        int val = (int)word[i] - 48;
-        int numSign = (int)word[i];
-        if (numSign != 35) {
-            if (temp->link[val] != NULL) {
-                temp = temp->link[val];
-                if (i == (strlen(word) - 1)) {
-                    if (temp->string != NULL) {
-                        printf("    '%s'\n", temp->string);
-                    } else {
-                        printf("    Not found in current dictionary.\n");
-                        break;
-                    }
-                }
-            } else {
-                printf("    Not found in current dictionary.\n");
-                break;
-            }
-        } else if (numSign == 35) {
-            if (temp->link[0] != NULL) {
-                temp = temp->link[0];
-                if (i == (strlen(word) - 1)) {
-                    printf("    '%s'\n", temp->string);
-                }
-            } else {
-                printf("    There are no more T9onyms\n");
-                printf("\n");
-                break;
-            }
-        } else {
-            printf("    Not found in current dictionary.\n");
-            break;
-        }
-    }
-}
-
+// Deallocates memory used by the program.
 void freeTree(struct Node* root) {
     if (root != NULL) {
         for (int i = 0; i < 10; i++) {
@@ -153,24 +80,8 @@ void freeTree(struct Node* root) {
     }
 }
 
-// return a new node containing integer x and a null tail field
-struct Node* form_node(int x, char* word) {
-    struct Node* ans = (struct Node*)malloc(sizeof(struct Node));
-    if (word != NULL) {
-        ans->string = (char*)malloc(sizeof(char)*strlen(word));
-        //ans->string = strncpy(ans->string, word, strlen(word));
-        strncpy(ans->string, word, strlen(word));
-    } else {
-        ans->string = NULL;
-    }
-    ans->val = x;
-    int i;
-    for (i = 0; i < 10; i++) {
-        ans->link[i] = NULL;
-    }
-    return ans;
-}
-
+// Passes in a letter and returns the integer
+// value associated with that letter.
 int whichDigit(char letter) {
     int digit[26];
     digit[0] = 2;
@@ -202,9 +113,12 @@ int whichDigit(char letter) {
     return digit[letter - 97];
 }
 
+// Trims the string that's passed in.
 char *trim(char *s) {
     int i = strlen(s) - 1;
     if ((i > 0) && (s[i] == '\n'))
         s[i] = '\0';
     return s;
 }
+
+#endif
